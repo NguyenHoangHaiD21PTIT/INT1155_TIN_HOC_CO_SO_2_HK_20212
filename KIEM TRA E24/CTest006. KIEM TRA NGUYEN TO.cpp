556bf6cd171440875df9879ccf03dfd6d4b4e053
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-03-22 07:32:26 +0700

    Add files via upload

---
+++ KIEM TRA E24/CTest006. KIEM TRA NGUYEN TO.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int check(int n){
+	if(n<=1) return 0;
+	for(int i = 2;i*i<=n;i++){
+		if(n%i==0) return 0;
+	}
+	return 1;
+}
+int main(){
+	int n, m;
+	scanf("%d%d", &n, &m);
+	int a[n][m];
+	for(int i = 0;i<n;i++){
+		for(int j = 0;j<m;j++) scanf("%d", &a[i][j]);
+	}
+	for(int i = 0;i<n;i++){
+		for(int j = 0;j<m;j++){
+			if(check(a[i][j])) printf("1 ");
+			else printf("0 ");
+		}
+		printf("\n");
+	}
+}
+	
---
+++ KIEM TRA E24/CTest021. SO NGUYEN TO KHAC NHAU.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int check(int n){
+	if(n<=1) return 0;
+	for(int i = 2;i*i<=n;i++){
+		if(n%i==0) return 0;
+	}
+	return 1;
+}
+void sx(int a[], int n){
+	for(int i=0;i<n-1;i++){
+		for(int j = i+1;j<n;j++){
+			if(a[i]>a[j]){
+				int tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+			}
+		}
+	}
+}
+int main(){
+	int t; 
+	scanf("%d", &t);
+	while(t--){
+		int n; scanf("%d", &n);
+		int a[n], res[10000], cnt[10000], dem = 0;
+		for(int i = 0;i<2000;i++) cnt[i] = 0;
+		for(int i = 0;i<n;i++) scanf("%d", &a[i]);
+		for(int i = 0;i<n;i++){
+			if(check(a[i]) && !cnt[a[i]]){
+				res[dem] = a[i];
+				cnt[a[i]] = 1;
+				dem++;
+			}
+		}
+		sx(res, dem);
+		for(int i = 0;i<dem;i++) printf("%d ", res[i]);
+		printf("\n");
+	}
+}
+/*
+2
+9
+2 7 7 2 12 8 5 4 888
+9
+2 7 7 2 12 8 5 4 888
+*/
