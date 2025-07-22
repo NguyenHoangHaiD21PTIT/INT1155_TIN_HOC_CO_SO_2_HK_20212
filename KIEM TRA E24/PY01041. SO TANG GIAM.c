4e849c4e55d05326398afa7f1650db1c0ab7d183
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-22 09:50:40 +0700

    Update PY01041. SO TANG GIAM.c

--- KIEM TRA E24/PY01041. SO TANG GIAM.c
+++ KIEM TRA E24/PY01041. SO TANG GIAM.c
@@ -1,43 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-
-int check(char s[]) {
-    int cnt1 = 0, cnt2 = 0, last_1 = -1;
-    int length = strlen(s);
-    if (length < 3) return 0; 
-    for (int i = 1; i < length; i++) {
-        if (s[i] > s[i-1]) cnt1++;
-        else {
-            if (s[i] == s[i-1]) return 0;  
-            else {
-                if (cnt1 == 0) return 0; 
-                else {
-                    last_1 = i;
-                    break;
-                }
-            }
-        }
+#include <stdbool.h>
+bool checktang(char s[], int l, int r) {
+    for (int i = l; i < r; i++) {
+        if (s[i] >= s[i + 1]) return false;
     }
-    if (cnt1 == 0) return 0;  
-    for (int i = last_1; i < length; i++) {
-        if (s[i] < s[i-1]) cnt2++;
-        else {
-            if (s[i] == s[i-1]) return 0; 
-            else return 0; 
-        }
-    }
-    if (cnt2 == 0) return 0;
-    return 1;  
+    return true;
 }
-
+bool checkgiam(char s[], int l, int r) {
+    for (int i = l; i < r; i++) {
+        if (s[i] <= s[i + 1]) return false;
+    }
+    return true;
+}
+bool check(char s[]) {
+    int n = strlen(s);
+    if (n < 3) return false;
+    for (int i = 1; i < n - 1; i++) {
+        if (checktang(s, 0, i) && checkgiam(s, i, n - 1)) return true;
+    }
+    return false;
+}
 int main() {
     int t;
-    scanf("%d", &t);  
+    scanf("%d", &t);
     while (t--) {
-        char s[100];  
-        scanf("%s", s);  
+        char s[1005];
+        scanf("%s", s);
         if (check(s)) printf("YES\n");
         else printf("NO\n");
     }
+    return 0;
 }
-
