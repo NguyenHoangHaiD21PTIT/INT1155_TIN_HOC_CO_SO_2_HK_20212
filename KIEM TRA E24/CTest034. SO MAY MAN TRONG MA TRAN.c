47d49eba09137d6d537d6b588d826116e24306ed
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-24 12:09:50 +0700

    Update CTest034. SO MAY MAN TRONG MA TRAN.c

--- KIEM TRA E24/CTest034. SO MAY MAN TRONG MA TRAN.c
+++ KIEM TRA E24/CTest034. SO MAY MAN TRONG MA TRAN.c
@@ -2,27 +2,28 @@
 int main() {
     int hang, cot;
     scanf("%d %d", &hang, &cot);
-    int a[100][100]; 
+    int a[100][100];
     for (int i = 0; i < hang; i++) {
         for (int j = 0; j < cot; j++) scanf("%d", &a[i][j]);
     }
-    int Min = 20000, Max = -1;
+    int Min = 1e9, Max = -1e9;
     for (int i = 0; i < hang; i++) {
         for (int j = 0; j < cot; j++) {
             if (a[i][j] < Min) Min = a[i][j];
             if (a[i][j] > Max) Max = a[i][j];
         }
     }
-    int res = Max - Min;
-    int check = 0;
-    printf("%d\n", res);
+    int res = Max - Min, found = 0;
     for (int i = 0; i < hang; i++) {
         for (int j = 0; j < cot; j++) {
             if (a[i][j] == res) {
+                if (!found) {
+                    printf("%d\n", res);
+                    found = 1;
+                }
                 printf("Vi tri [%d][%d]\n", i, j);
-                check = 1;
             }
         }
     }
-    if(!check) printf("NOT FOUND");
+    if (!found) printf("NOT FOUND");
 }
